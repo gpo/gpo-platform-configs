dc093dc2fa77b5e19a66f706044fe6e49266f3c8
Ian Edington <ianedington@gpo.ca>
2024-07-21 17:15:16 -0400

    Create two action secrets in secure.gpo.ca

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ This repo is WIP. It goes along with a design proposal to adopt Terraform.
 Design Doc:
 https://docs.google.com/document/d/1-2-MSpd-g_i5UjxHVkZW8wpKUZORjLrzd8A-UhW2XAY/edit
 
+
+# Requirements
+
+* gh with an authenticated session
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "secure_gpo_ca_secret_ssh_user" {
+  description = "SSH user for secure.gpo.ca"
+  type        = string
+}
+
+variable "secure_gpo_ca_secret_ssh_host" {
+  description = "SSH host for secure.gpo.ca"
+  type        = string
+}
