df5ab25c818ec005bbc3e66915679dc7bc243f27
Ian Edington <ianedington@gpo.ca>
2024-07-21 17:23:48 -0400

    fix owner

--- main.tf
+++ main.tf
@@ -9,16 +9,18 @@ terraform {
 
 # Configure the GitHub Provider
 # requires authorized gh
-provider "github" {}
+provider "github" {
+  owner = "gpo"
+}
 
 resource "github_actions_secret" "SSH_USER" {
-  repository =  "gpo/secure.gpo.ca"
+  repository =  "secure.gpo.ca"
   secret_name      = "SSH_USER"
   plaintext_value = var.secure_gpo_ca_secret_ssh_user
 }
 
 resource "github_actions_secret" "SSH_HOST" {
-  repository =   "gpo/secure.gpo.ca"
+  repository =   "secure.gpo.ca"
   secret_name      = "SSH_HOST"
   plaintext_value = var.secure_gpo_ca_secret_ssh_host
 }
