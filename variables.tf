544731859177536a22bd5b5a78bbcce41409abf0
Ian Edington <ianedington@gpo.ca>
2024-07-22 00:01:12 -0400

    Add ssh keys to action secrets in secure.gpo.ca

--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "secure_gpo_ca_secret_ssh_host" {
   description = "SSH host for secure.gpo.ca"
   type        = string
 }
+
+variable "secure_gpo_ca_secret_ssh_private_key" {
+    description = "SSH private key for secure.gpo.ca"
+    type        = string
+}
+
+variable "secure_gpo_ca_secret_ssh_public_key" {
+    description = "SSH public key for secure.gpo.ca"
+    type        = string
+}
