e5dc59faf21ff2613ace6703ea86066d39980eae
Ian Edington <ianedington@gpo.ca>
2024-07-22 10:49:59 -0400

    github labels: import existing labels

--- github.tf
+++ github.tf
@@ -42,3 +42,61 @@ resource "github_actions_secret" "SSH_PUBLIC_KEY" {
   secret_name      = "SSH_PUBLIC_KEY"
   plaintext_value  = var.secure_gpo_ca_secret_ssh_public_key
 }
+
+resource "github_issue_labels" "secure_gpo_ca" {
+  repository = "secure.gpo.ca"
+
+  label {
+    color       = "0075ca"
+    description = "Improvements or additions to documentation"
+    name        = "documentation"
+  }
+
+  label {
+    color       = "008672"
+    description = "Extra attention is needed"
+    name        = "help wanted"
+  }
+
+  label {
+    color       = "7057ff"
+    description = "Good for newcomers"
+    name        = "good first issue"
+  }
+
+  label {
+    color       = "a2eeef"
+    description = "New feature or request"
+    name        = "enhancement"
+  }
+
+  label {
+    color       = "cfd3d7"
+    description = "This issue or pull request already exists"
+    name        = "duplicate"
+  }
+
+  label {
+    color       = "d73a4a"
+    description = "Something isn't working"
+    name        = "bug"
+  }
+
+  label {
+    color       = "d876e3"
+    description = "Further information is requested"
+    name        = "question"
+  }
+
+  label {
+    color       = "e4e669"
+    description = "This doesn't seem right"
+    name        = "invalid"
+  }
+
+  label {
+    color       = "ffffff"
+    description = "This will not be worked on"
+    name        = "wontfix"
+  }
+}
