dd72ca96a7565a7f66d2a0a5fffd991057e7c92d
Ian Edington <ianedington@gpo.ca>
2024-07-29 17:35:24 -0400

    terraform state: migrate to aws

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.60"
+    }
+
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+
+  #############################################################
+  ## AFTER RUNNING TERRAFORM APPLY (WITH LOCAL BACKEND)
+  ## YOU WILL UNCOMMENT THIS CODE THEN RERUN TERRAFORM INIT
+  ## TO SWITCH FROM LOCAL BACKEND TO REMOTE AWS BACKEND
+  #############################################################
+  # backend "s3" {
+  #   bucket         = "terraform-state"
+  #   key            = "03-basics/import-bootstrap/terraform.tfstate"
+  #   region         = "ca-central-1"
+  #   dynamodb_table = "terraform-state-locks"
+  #   encrypt        = true
+  # }
+}
