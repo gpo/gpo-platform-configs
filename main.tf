66381d1d71f2097582e2028560a630490a755e2a
Ian Edington <ianedington@gpo.ca>
2024-08-01 22:50:22 -0400

    migrate state

--- main.tf
+++ main.tf
@@ -11,16 +11,11 @@ terraform {
     }
   }
 
-  #############################################################
-  ## AFTER RUNNING TERRAFORM APPLY (WITH LOCAL BACKEND)
-  ## YOU WILL UNCOMMENT THIS CODE THEN RERUN TERRAFORM INIT
-  ## TO SWITCH FROM LOCAL BACKEND TO REMOTE AWS BACKEND
-  #############################################################
-  # backend "s3" {
-  #   bucket         = "gpo-terraform-state"
-  #   key            = "03-basics/import-bootstrap/terraform.tfstate"
-  #   region         = "ca-central-1"
-  #   dynamodb_table = "terraform-state-locks"
-  #   encrypt        = true
-  # }
+  backend "s3" {
+    bucket         = "gpo-terraform-state"
+    key            = "03-basics/import-bootstrap/terraform.tfstate"
+    region         = "ca-central-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
 }
