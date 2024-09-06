2ef255de21a86e329dc6235711e15c99b17a90af
Ian Edington <ianedington@gpo.ca>
2024-09-05 22:33:48 -0400

    feat: remove s3 policy from drupal

--- s3_buckets.tf
+++ s3_buckets.tf
@@ -2,20 +2,3 @@ resource "digitalocean_spaces_bucket" "drupal" {
   name   = "drupal"
   region = "nyc3"
 }
-
-resource "digitalocean_spaces_bucket_policy" "example_policy" {
-  bucket = digitalocean_spaces_bucket.drupal.name
-  region = digitalocean_spaces_bucket.drupal.region
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid" : "PrivateAccess",
-        "Effect" : "Deny",
-        "Principal" : "*",
-        "Action" : "s3:GetObject",
-        "Resource" : "arn:aws:s3:::${digitalocean_spaces_bucket.drupal.name}/private/*"
-      }
-    ]
-  })
-}
