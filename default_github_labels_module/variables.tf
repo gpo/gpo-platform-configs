bbc8ca5791de9b3b19f354ad6d0a126ee6012998
Ian Edington <ianedington@gpo.ca>
2024-10-23 22:09:10 -0400

    feat: add repo specific labels (#21)

--- default_github_labels_module/variables.tf
+++ default_github_labels_module/variables.tf
@@ -2,3 +2,13 @@ variable "repository" {
   description = "The name of the repository"
   type        = string
 }
+
+variable "labels" {
+  description = "List of labels [{name, description, color?}]"
+  type = list(object({
+    name        = string
+    description = optional(string)
+    color       = optional(string)
+  }))
+  default = []
+}
