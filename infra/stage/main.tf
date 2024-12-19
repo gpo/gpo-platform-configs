2f5f5b7d39090450fb3ad1ac5685430baef5005b
rob salmond <rob@salmond.ca>
2024-12-19 16:46:42 -0500

    initial pass

--- infra/stage/main.tf
+++ infra/stage/main.tf
@@ -1,7 +1,12 @@
-module "eks" {
-  source = "../../modules/infra/eks"
-}
+/*module "eks" {
+  source      = "../../modules/infra/eks"
+  name        = local.project_name
+  environment = local.environment
+  cluster_subnet_ids = [module.vpc.public_subnet_id, module.vpc.private_subnet_id]
+}*/
 
 module "vpc" {
-  source = "../../modules/infra/vpc"
+  source      = "../../modules/infra/vpc"
+  name        = local.project_name
+  environment = local.environment
 }
---
+++ modules/infra/eks/iam.tf
@@ -0,0 +1,24 @@
+resource "aws_iam_role" "cluster" {
+  name = "${var.name}-${var.environment}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster" {
+  # use canned EKS cluster policy
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
