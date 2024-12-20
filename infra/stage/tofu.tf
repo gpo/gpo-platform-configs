d580fae55a59b4ce375df98c8bcacc0f2d965ac2
rob salmond <rob@salmond.ca>
2024-12-20 13:00:04 -0500

    profile name

--- infra/stage/tofu.tf
+++ infra/stage/tofu.tf
@@ -23,7 +23,7 @@ terraform {
     region         = "ca-central-1"
     dynamodb_table = "terraform-state-locks"
     encrypt        = true
-    profile        = "gpo"
+    profile        = "gpo-stage"
   }
 }
 
