2fc0dd4d5e2ea21ec8e055d41369de779cf8bad3
rob salmond <rob@salmond.ca>
2025-01-24 13:42:48 -0500

    isolate bootstrap modules

--- bootstrap/iam.tf
+++ bootstrap/iam.tf
@@ -1,5 +1,5 @@
 module "iam_users" {
-  source          = "../modules/infra/iam_users"
+  source          = "../modules/bootstrap/iam_users"
   iam_admin_users = local.iam_admin_users
   iam_eks_users   = local.iam_eks_users
 }
