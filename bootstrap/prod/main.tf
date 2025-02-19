40d16526e8f4e90042d9474c294cd099c1cb5ddd
rob salmond <rob@salmond.ca>
2025-02-19 16:02:33 -0500

    sync stage->prod

--- bootstrap/prod/main.tf
+++ bootstrap/prod/main.tf
@@ -2,6 +2,7 @@ module "iam_users" {
   source          = "../../modules/bootstrap/iam_users"
   iam_admin_users = local.iam_admin_users
   iam_eks_users   = local.iam_eks_users
+  environment     = local.environment
 }
 
 module "state_bucket" {
