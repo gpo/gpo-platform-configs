74082a6b7112159145022527941bb72372e30b24
rob salmond <rob@salmond.ca>
2025-10-07 15:28:09 -0400

    bring bi module into app state, reference sops secrets

--- app/stage/main.tf
+++ app/stage/main.tf
@@ -8,3 +8,12 @@ module "drupal" {
 module "legacy_logging" {
   source = "../../modules/app/legacy_logging"
 }
+
+module "bi_dashboards" {
+  source         = "../../modules/app/bi_dashboards"
+  mysql_password = data.sops_file.environment_secrets.data["database_pass"]
+  mysql_username = data.sops_file.environment_secrets.data["database_user"]
+  mysql_host     = data.sops_file.environment_secrets.data["database_host"]
+  gcp_project    = data.sops_file.environment_secrets.data["gcp_bi_project_id"]
+  gcp_region     = "northamerica-northeast2"
+}
--- infra/stage/main.tf
+++ infra/stage/main.tf
@@ -22,10 +22,3 @@ module "ecr" {
   source       = "../../modules/infra/ecr"
   repositories = local.repositories
 }
-
-module "gcp" {
-  source = "../../modules/infra/gcp"
-  # TODO: Figure out how to pass the MySQL sensitive values in as input
-  # params. Will this involve integrating with SOPS as our secrets source?
-  # (I saw `data.sops_file` elsewhere in this codebase.)
-}
