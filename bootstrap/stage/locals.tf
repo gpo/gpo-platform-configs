11d6bacf801b0ee2114b2c2f6b2a13f13f796c1a
rob salmond <rob@salmond.ca>
2025-10-07 17:16:45 -0400

    adding matt welke (#94)

--- bootstrap/stage/locals.tf
+++ bootstrap/stage/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  iam_admin_users = ["rsalmond", "ianedington", "verdird"]
+  iam_admin_users = ["rsalmond", "ianedington", "verdird", "mattw"]
   iam_eks_users   = ["pnovikov"]
   environment     = "stage"
 }
