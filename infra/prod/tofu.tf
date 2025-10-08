ede6a1c6478ac6c1bfd7370511ddb966299117a6
Matt Welke <mattwelke@gpo.ca>
2025-10-08 12:04:24 -0400

    Remove use of GCP provider from prod

--- infra/prod/tofu.tf
+++ infra/prod/tofu.tf
@@ -15,11 +15,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.81"
     }
-
-    google = {
-      source  = "hashicorp/google"
-      version = "6.8.0"
-    }
   }
 
   backend "s3" {
