71f1c649cb9ac6a22c8df8234e0e2a1f3fba7508
rob salmond <rob@salmond.ca>
2025-12-05 13:55:43 -0500

    grant missing role

--- modules/infra/gcp/gke/iam.tf
+++ modules/infra/gcp/gke/iam.tf
@@ -1,5 +1,11 @@
-resource "google_project_iam_member" "main" {
+resource "google_project_iam_member" "registry_reader" {
   project = data.google_client_config.current.project
   role    = "roles/artifactregistry.reader"
+  member  = google_service_account.main.member
+}
+
+resource "google_project_iam_member" "node_svc_account" {
+  project = data.google_client_config.current.project
+  role    = "roles/container.defaultNodeServiceAccount"
   member  = google_service_account.main.member
 }
