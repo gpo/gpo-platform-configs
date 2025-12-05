8fed795f3440d840c81308da4b59684302d7a79a
rob salmond <rob@salmond.ca>
2025-12-05 13:56:09 -0500

    add svc deps

--- modules/infra/gcp/gar/main.tf
+++ modules/infra/gcp/gar/main.tf
@@ -1,6 +1,15 @@
+resource "google_project_service" "artifact_registry" {
+  service            = "artifactregistry.googleapis.com"
+  disable_on_destroy = false # don't disable / renable the API on every tf destroy / apply
+}
+
 resource "google_artifact_registry_repository" "main" {
   location      = "northamerica-northeast2" # toronto
   repository_id = "gpo"
   description   = "repo for gpo containers"
   format        = "DOCKER"
+
+  depends_on = [
+    google_project_service.artifact_registry
+  ]
 }
