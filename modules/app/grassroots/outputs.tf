6e98be64ab7496f0e4509e280e281b279ed66a9c
rob salmond <rob@salmond.ca>
2025-12-13 15:19:49 -0500

    moving mountains

--- modules/app/grassroots/outputs.tf
+++ modules/app/grassroots/outputs.tf
@@ -1,10 +1,3 @@
-output "oauth_client" {
-  value = {
-    id     = google_iam_oauth_client.grassroots.oauth_client_id
-    secret = google_iam_oauth_client_credential.grassroots.client_secret
-  }
-}
-
 output "hostname" {
   value = cloudflare_record.grassroots.name
 }
