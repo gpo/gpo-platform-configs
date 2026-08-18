7386b2c361ff4b1fc9c64d7e6e2b8031338aa826
rob salmond <rob@salmond.ca>
2026-08-18 13:36:51 -0400

    spilt gateway outputs by hostname

--- tf/app/stage/outputs.tf
+++ tf/app/stage/outputs.tf
@@ -56,7 +56,8 @@ output "gateway" {
       hostname = data.terraform_remote_state.infra.outputs.cloudflare_zone_gpo_tools.zone
     }
     gateway = {
-      hostname = data.terraform_remote_state.infra.outputs.cloudflare_zone_gpo_tools.zone
+      gpo_tools_hostname = data.terraform_remote_state.infra.outputs.cloudflare_zone_gpo_tools.zone
+      gpo_gear_hostname  = data.terraform_remote_state.infra.outputs.cloudflare_zone_gpo_gear.zone
     }
     redirect = {
       hostname = data.terraform_remote_state.infra.outputs.cloudflare_zone_gpo_tools.zone
