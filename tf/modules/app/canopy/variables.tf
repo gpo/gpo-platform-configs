474e591b71473790f3054e4263d84cff84a13d83
rob salmond <rob@salmond.ca>
2026-08-21 18:11:31 -0400

    add canopy database

--- tf/modules/app/canopy/variables.tf
+++ tf/modules/app/canopy/variables.tf
@@ -6,6 +6,36 @@ variable "environment" {
   }
 }
 
+variable "database_tier" {
+  # see: gcloud sql tiers list
+  type        = string
+  description = "CloudSQL DB Tier to deploy."
+}
+
+variable "database_edition" {
+  type        = string
+  description = "CloudSQL edition to deploy."
+  validation {
+    condition     = contains(["ENTERPRISE", "ENTERPRISE_PLUS"], var.database_edition)
+    error_message = "database_edition must be either \"ENTERPRISE\" or \"ENTERPRISE_PLUS\"."
+  }
+}
+
+variable "vpc_network_name" {
+  type        = string
+  description = "Name of the VPC in which to place the Canopy database."
+}
+
+variable "zone" {
+  type        = string
+  description = "GCP availability zone in which to place the Canopy DB."
+}
+
+variable "region" {
+  type        = string
+  description = "GCP region in which to place the Canopy DB."
+}
+
 variable "ingress_ip_address" {
   type        = string
   description = "The GKE Ingress IP."
--- tf/modules/infra/gcp/gke/networking.tf
+++ tf/modules/infra/gcp/gke/networking.tf
@@ -1,3 +1,10 @@
+# our primary VPC - everything lives in here
+resource "google_compute_network" "main" {
+  depends_on              = [google_project_service.compute]
+  name                    = "${var.name}-${var.environment}"
+  auto_create_subnetworks = true
+}
+
 # We need at least one load balancer provisioned by the Kubernetes resources we
 # create in the GKE cluster. It's better to reserve a static IP for the load
 # balancer. This is that static IP.
@@ -8,4 +15,20 @@ resource "google_compute_global_address" "gke_ingress_ip" {
   depends_on = [
     google_project_service.compute
   ]
+}
+
+# this is a block of IP addresses in our VPC that we will allow Google to use
+# for connecting us to CloudSQL / Redis / Whatever managed services we're using.
+resource "google_compute_global_address" "managed_services" {
+  name          = "gcp-peering-private-ip-range"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.main.id
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.main.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.managed_services.name]
 }
